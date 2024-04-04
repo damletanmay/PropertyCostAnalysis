474cd7fccaa286c729d1e0a49beb7d4c9565b681
VatsalMori09 <160768460+VatsalMori09@users.noreply.github.com>
2024-04-03 20:13:43 -0400

    Update DataValidation.java

--- src/main/java/propertyCost/DataValidation.java
+++ src/main/java/propertyCost/DataValidation.java
@@ -1,24 +1,25 @@
 package propertyCost;
 
-import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class DataValidation {
 
-    // Validate Canadian postal code with restricted letters
-    public static boolean isValidPostalCode(String postalCode) {
-        String regex = "^[ABCEGHJKLMNPRSTVXYabceghjklmnprstvxy]\\d[A-Za-z] ?\\d[A-Za-z]\\d$";
-        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
-        Matcher matcher = pattern.matcher(postalCode);
-        return matcher.matches();
+    // Method to check & validat Canadian postal_code 
+    public static boolean isValidPostalCode(String postal_Code) {
+        // Reg_expre for validating postal code format
+        String regexPostalCode = "^[ABCEGHJKLMNPRSTVXYabceghjklmnprstvxy]\\d[A-Za-z] ?\\d[A-Za-z]\\d$";
+        Pattern patternPostalCode = Pattern.compile(regexPostalCode, Pattern.CASE_INSENSITIVE);
+        Matcher match = patternPostalCode.matcher(postal_Code);
+        return match.matches(); // Return true if the postal code matches the pattern
     }
 
-    // Validate if it is a city 
-    public static boolean isCity(String city) {
-        String regex = "[A-Za-z ]+";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(city);
-        return matcher.matches();
+    // Method to validate if a string represents a city
+    public static boolean isCity(String cities) {
+        // Reg-ex for validatng city name format
+        String regexCity = "[A-Za-z ]+";
+        Pattern patternCity = Pattern.compile(regexCity);
+        Matcher match = patternCity.matcher(cities);
+        return match.matches(); // Return true if the string represents a valid city name
     }
 }
